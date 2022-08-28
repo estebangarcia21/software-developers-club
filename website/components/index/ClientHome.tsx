e559eea97d25e6a51d140a6b936e94d9e6e43688
Esteban Garcia <estebangarcia2121@gmail.com>
2022-08-27 23:13:16 -0700

    Fix imports and css background

--- website/components/index/ClientHome.tsx
+++ website/components/index/ClientHome.tsx
@@ -1,4 +1,3 @@
-import type { NextPage } from 'next';
 import { animated, SpringValues } from 'react-spring';
 import useAnimations from '../../hooks/index/useAnimations';
 import styles from '../../styles/Home.module.scss';
